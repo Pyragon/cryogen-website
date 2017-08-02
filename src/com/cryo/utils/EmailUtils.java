93d646ab97893615e9f0b3a6468146ed4619ddff
Cody Thompson <eldo.imo.rs@hotmail.com>
2017-08-01 23:06:17 -0700

    Update recovery email

--- src/com/cryo/utils/EmailUtils.java
+++ src/com/cryo/utils/EmailUtils.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 import com.cryo.Website;
 import com.cryo.db.impl.EmailConnection;
+import com.cryo.db.impl.RecoveryConnection;
 import com.cryo.modules.account.AccountDAO;
 import com.cryo.modules.account.AccountUtils;
 
@@ -25,13 +26,28 @@ import com.cryo.modules.account.AccountUtils;
 public class EmailUtils {
 	
 	public static void sendVerificationEmail(String username, String email) {
-		final String email_user = "pyragon.eldo@gmail.com";
-		final String password = (String) Website.getProperties().get("emailpass");
 		final String random = RandomStringUtils.random(20, true, true);
 		final AccountDAO account = AccountUtils.getAccount(username);
 		if(account == null)
 			return;
 		EmailConnection.connection().handleRequest("add-verify", username, email, random);
+		sendEmail(email, "Cryogen - Email verification almost complete!", "Hello, "+AccountUtils.getDisplayName(account)+"!"
+				+ "\n\nYour verification is nearly complete. Click or paste this link into your browser to verify your email! http://cryogen-rsps.com/account?action=verify&id="+random+"\n\nThis link will expire in 24 hours.");
+	}
+	
+	public static void sendRecoveryEmail(String username, String id, String email) {
+		final String random = RandomStringUtils.random(20, true, true);
+		final AccountDAO account = AccountUtils.getAccount(username);
+		if(account == null)
+			return;
+		RecoveryConnection.connection().handleRequest("add-email-rec", id, random);
+		sendEmail(email, "Cryogen - Password Recovery!",
+				"Hello, "+AccountUtils.getDisplayName(account)+", a password recovery attempt has been made on your account.\n\nIf you did not make this request, click here immediately!\n\nOtherwise follow this link to reset your password: http://cryogen-rsps.com/recover_final?method=email&id="+random);
+	}
+	
+	public static void sendEmail(String email, String subject, String message) {
+		final String email_user = (String) Website.getProperties().get("email");
+		final String password = (String) Website.getProperties().get("emailpass");
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
@@ -47,15 +63,14 @@ public class EmailUtils {
 
 		try {
 
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("webm@gmail.com"));
-			message.setRecipients(Message.RecipientType.TO,
+			Message mime = new MimeMessage(session);
+			mime.setFrom(new InternetAddress("webmaster@cryogen-rsps.com"));
+			mime.setRecipients(Message.RecipientType.TO,
 				InternetAddress.parse(email));
-			message.setSubject("Cryogen - Email verification almost complete!");
-			message.setText("Hello, "+AccountUtils.getDisplayName(account)+"!"
-				+ "\n\nYour verification is nearly complete. Click or paste this link into your browser to verify your email! http://cryogen-rsps.com/account?action=verify&id="+random+"\n\nThis link will expire in 24 hours.");
+			mime.setSubject(subject);
+			mime.setText(message);
 
-			Transport.send(message);
+			Transport.send(mime);
 
 		} catch (MessagingException e) {
 			throw new RuntimeException(e);
