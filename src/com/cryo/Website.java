109d82a58206c2970fa1a99f4bbabe93395442d4
Cody <cody@cryogen-rsps.com>
2018-04-05 06:00:24 -0700

    Add COR access

--- src/com/cryo/Website.java
+++ src/com/cryo/Website.java
@@ -113,6 +113,30 @@ public class Website {
 		PaypalManager.createAPIContext();
 		staticFiles.externalLocation("source/");
 		staticFiles.expireTime(0); // ten minutes
+		options("/*",
+		        (request, response) -> {
+
+		            String accessControlRequestHeaders = request
+		                    .headers("Access-Control-Request-Headers");
+		            if (accessControlRequestHeaders != null) {
+		                response.header("Access-Control-Allow-Headers",
+		                        accessControlRequestHeaders);
+		            }
+
+		            String accessControlRequestMethod = request
+		                    .headers("Access-Control-Request-Method");
+		            if (accessControlRequestMethod != null) {
+		                response.header("Access-Control-Allow-Methods",
+		                        accessControlRequestMethod);
+		            }
+
+		            return "OK";
+		        });
+
+		before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+		get(SamsungTVModule.PATH, (req, res) -> {
+			return new SamsungTVModule(this).decodeRequest(req, res, RequestType.GET);
+		});
 		get(IndexModule.PATH, (req, res) -> {
 			return new IndexModule(this).decodeRequest(req, res, RequestType.GET);
 		});
