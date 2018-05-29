970b3081365f7044fb194b5edb69eed75a6ce84c
Pyragon <cody@cryogenrsps.com>
2018-05-29 14:25:38 -0700

    Add created by filter, and add search instructions to announcements

---
+++ src/com/cryo/modules/search/impl/CreatedByFilter.java
@@ -0,0 +1,28 @@
+package com.cryo.modules.search.impl;
+
+import com.cryo.modules.search.Filter;
+
+public class CreatedByFilter extends Filter {
+
+	public CreatedByFilter() {
+		super("createdby");
+	}
+
+	@Override
+	public String getFilter(String mod) {
+		return "username LIKE ?";
+	}
+
+	@Override
+	public boolean setValue(String mod, String value) {
+		value = value.toLowerCase();
+		this.value = "%"+value+"%";
+		return true;
+	}
+
+	@Override
+	public boolean appliesTo(String mod) {
+		return isMod(mod, "announcements");
+	}
+
+}
