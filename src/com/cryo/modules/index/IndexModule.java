59a35849045f567efb0a450906ed8dd1142a0cca
Cody Thompson <eldo.imo.rs@hotmail.com>
2020-10-30 00:21:24 -0700

    Readd news forum posts to homepage

--- src/com/cryo/modules/index/IndexModule.java
+++ src/com/cryo/modules/index/IndexModule.java
@@ -2,10 +2,14 @@ package com.cryo.modules.index;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.cryo.Website;
 import com.cryo.Website.RequestType;
 import com.cryo.cache.CachedItem;
+import com.cryo.db.impl.ForumConnection;
+import com.cryo.entities.forums.Post;
 import com.cryo.modules.WebModule;
 import com.cryo.utils.DateUtils;
 
@@ -37,6 +41,14 @@ public class IndexModule extends WebModule {
 		if(cache == null)
 			return Website.error("Error loading HS Cache.");
 		model.put("hsusers", cache.getCachedData("mini-list"));
+		//Latest threads
+		//List of 'Post' (first post of last... 4/5 threads?)
+		List<Post> posts = ForumConnection.connection()
+				.selectList("threads", "archived=0 AND forum_id IN (1,2)", "ORDER BY added DESC LIMIT 4", com.cryo.entities.forums.Thread.class)
+				.stream()
+				.map(t -> t.getFirstPost())
+				.collect(Collectors.toList());
+		model.put("newsPosts", posts);
 		model.put("redirect", "/");
 		String html = null;
 		try {
