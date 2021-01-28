cd51cc379f10db5f4fce31b105e09cc1f543ee59
Cody Thompson <eldo.imo.rs@hotmail.com>
2021-01-28 15:12:15 -0800

    Change from fingerprintjs to just storing uuid locally

--- public/js/utils/spa.js
+++ public/js/utils/spa.js
@@ -39,7 +39,8 @@ function post(link, data, selector, cb) {
         selector = null;
     }
     selector = selector || '#main-content';
-    data.visitorId = visitorId;
+    if (visitorId)
+        data.visitorId = visitorId;
     if (!visitorId) {
         setTimeout(() => {
             post(link, data, selector, cb);
@@ -98,10 +99,39 @@ function parseJSON(data) {
     return data;
 }
 
-function initFingerprintJS() {
-    FingerprintJS.load().then(fp => {
-        fp.get().then(result => {
-            visitorId = result.visitorId;
-        });
-    });
+function createUUID() {
+    if (!storageAvailable('localStorage'))
+        return false;
+    if (window.localStorage.getItem('cryogen_uuid'))
+        visitorId = window.localStorage.getItem('cryogen_uuid');
+    else {
+        visitorId = uuid.v4();
+        window.localStorage.setItem('cryogen_uuid', visitorId);
+    }
+}
+
+createUUID();
+
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return e instanceof DOMException && (
+                // everything except Firefox
+                e.code === 22 ||
+                // Firefox
+                e.code === 1014 ||
+                // test name field too, because code might not be present
+                // everything except Firefox
+                e.name === 'QuotaExceededError' ||
+                // Firefox
+                e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
 }
