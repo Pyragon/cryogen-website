9cd2227546d97172e7677b6d95d4986ff9bffa2b
Cody Thompson <eldo.imo.rs@hotmail.com>
2022-02-01 15:08:38 -0800

    Restyle pinned threads

--- src/components/forums/threads/ThreadBlock.jsx
+++ src/components/forums/threads/ThreadBlock.jsx
@@ -4,18 +4,20 @@ import { Link } from 'react-router-dom';
 
 import DisplayUser from '../../utils/user/DisplayUser';
 
+import '../../../styles/forums/Thread.css';
+
 export default function ThreadBlock({ thread, index }) {
     return (
-        <div className="thread">
-            <div className="thread-header">
-                <div className="thread-title">
-                    { thread.pinned && <span className="fa fa-thumb-tack green" title='This thread has been pinned.' /> }
-                    { !thread.open && <span className="fa fa-lock red" title='This thread has been locked.'/> }
+        <div className={'thread '+(thread.pinned ? 'pinned' : '')}>
+            <div className='thread-header'>
+                <div className='thread-title'>
+                    { thread.pinned && <span className='fa fa-thumbtack green' title='This thread has been pinned.' /> }
+                    { !thread.open && <span className='fa fa-lock red' title='This thread has been locked.'/> }
                     <span>
-                        <Link to={`/forums/threads/${thread._id}`} className="link">{' '+thread.title}</Link>
+                        <Link to={`/forums/threads/${thread._id}`} className='link'>{' '+thread.title}</Link>
                     </span>
                 </div>
-                <div className="thread-info small">
+                <div className='thread-info small'>
                     <DisplayUser 
                         user={thread.author}
                         prefix='Started by '
@@ -23,16 +25,16 @@ export default function ThreadBlock({ thread, index }) {
                     />
                 </div>
             </div>
-            <div className="thread-last-post small">
+            <div className='thread-last-post small'>
                 <DisplayUser
                     user={thread.lastPost?.author}
                     prefix='Last post by '
                     suffix={ ', '+formatDate(thread.lastPost?.createdAt)}
                 />
             </div>
-            <div className="thread-view-info">
-                <p className="small">{'Replies: '+thread.postCount}</p>
-                <div className="small m-top-5">{'Views: '+thread.views}</div>
+            <div className='thread-view-info'>
+                <p className='small'>{'Replies: '+thread.postCount}</p>
+                <div className='small m-top-5'>{'Views: '+thread.views}</div>
             </div>
         </div>
     )
