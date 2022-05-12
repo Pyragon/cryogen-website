ce6f1e590071f0c36a93a86641d3ced538806784
Cody Thompson <eldo.imo.rs@hotmail.com>
2022-05-12 12:56:03 -0700

    Update usergroup page information from lorem ipsum

--- src/components/forums/admin/usergroups/Usergroups.jsx
+++ src/components/forums/admin/usergroups/Usergroups.jsx
@@ -10,9 +10,9 @@ import PageContext from '../../../../utils/contexts/PageContext';
 import NotificationContext from '../../../../utils/contexts/NotificationContext';
 
 const info = [
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+    'The following page can be used to create usergroups for the Cryogen Website and in-game',
+    'The rights set here correlate to in-game',
+    'Please be very careful editing old usergroups, as it will effect anyone in that usergroup'
 ];
 
 export default function Usergroups() {
